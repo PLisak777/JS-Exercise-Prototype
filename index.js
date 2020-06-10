fde19ff3d4f743c095e24b1dc33e973514d61fa7
Philip Lisak <phil.lisak@gmail.com>
2020-06-09 20:17:03 -0700

    MVP Complete

--- index.js
+++ index.js
@@ -9,16 +9,15 @@
 
 // EXAMPLE SOLUTION CODE:
 function Airplane(name) {
-  this.name = name;
-  this.isFlying = false;
+    this.name = name;
+    this.isFlying = false;
 }
-Airplane.prototype.takeOff = function () {
-  this.isFlying = true;
+Airplane.prototype.takeOff = function() {
+    this.isFlying = true;
 };
-Airplane.prototype.land = function () {
-  this.isFlying = false;
+Airplane.prototype.land = function() {
+    this.isFlying = false;
 };
-
 
 /*
 // 👇 COMPLETE YOUR WORK BELOW 👇
@@ -39,9 +38,41 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.speak = function personToString() {
+        return `Hi, my name is ${name}, and I am ${age} years old.`;
+    };
+    this.stomach = stomach;
 }
+
+Person.prototype.toString = function personToString() {
+    return `Hi, my name is ${this.name}, and I am ${this.age} years old.`;
+};
+
+let stomach = [];
+
+const phil = new Person({
+    name: "Phil",
+    age: 38
+});
+
+// console.log(phil);
+
+Person.prototype.eat = function(someFood) {
+    if (stomach.length < 10) {
+        stomach.push(someFood);
+    }
+    return stomach;
+};
+
+Person.prototype.poop = function() {
+    if (stomach.length >= 10) {
+        stomach.splice(0, stomach.length);
+    }
+    return `Whew, stinky!`;
+};
 
 /*
   TASK 2
@@ -57,9 +88,28 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = tank;
+    this.odometer = odometer;
 }
+
+let tank = 0;
+let odometer = 0;
+
+const Cruze = new Car({
+    model: "Cruze",
+    milesPerGallon: 26,
+    tank: tank,
+    odometer: odometer
+});
+
+console.log(Cruze);
+
+Car.prototype.fill = function(gallons) {
+    this.tank = this.tank + gallons;
+};
 
 /*
   TASK 3
@@ -68,28 +118,55 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+    Person.call(this, name, age, favoriteToy);
+    this.name = name;
+    this.age = age;
+    this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+const lucy = new Baby({
+    isChild: true,
+    name: "Lucy",
+    age: 5,
+    favoriteToy: "trains"
+});
+
+console.log(lucy);
+
+Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`;
+};
+
+lucy.play();
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Global Binding links 'this' to the entire page. Bad form.
+  2. Implicit Binding applies 'this' to the object it was created in. 
+  3. New Binding works inside a constructor function and applies to the object created by the constructor.
+  4. Explicit Binding is used when using .call or .apply to alter the values set by a constructor.
 */
 
-
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
-if (typeof exports !== 'undefined') {
-  module.exports = module.exports || {}
-  if (Airplane) { module.exports.Airplane = Airplane }
-  if (Person) { module.exports.Person = Person }
-  if (Car) { module.exports.Car = Car }
-  if (Baby) { module.exports.Baby = Baby }
+if (typeof exports !== "undefined") {
+    module.exports = module.exports || {};
+    if (Airplane) {
+        module.exports.Airplane = Airplane;
+    }
+    if (Person) {
+        module.exports.Person = Person;
+    }
+    if (Car) {
+        module.exports.Car = Car;
+    }
+    if (Baby) {
+        module.exports.Baby = Baby;
+    }
 }
